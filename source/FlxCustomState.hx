066e4b11ea3fdc026b9c4d6c8599606fc463ce16
kivers69 <georgechen1297@gmail.com>
2024-09-04 12:04:24 +0800

    fixed

--- source/FlxCustomState.hx
+++ source/FlxCustomState.hx
@@ -1,11 +1,11 @@
 package;
 
 class FlxCustomState extends FlxState {
-    public static var onCreate:Void -> Void;
-    public static var onCreatePost:Void -> Void;
+    public static var onCreate:() -> Void;
+    public static var onCreatePost:() -> Void;
 
-    public static var onUpdate:Void -> Void;
-    public static var onUpdatePost:Void -> Void;
+    public static var onUpdate:(Float) -> Void;
+    public static var onUpdatePost:(Float) -> Void;
 
     public var instance:FlxCustomState;
 
