c1d1ace97a4016f7738788e4e5c163e25efe30e4
kivers69 <georgechen1297@gmail.com>
2024-09-10 18:40:28 +0800

    Updated Tile

--- source/objects/Tile.hx
+++ source/objects/Tile.hx
@@ -1,15 +1,20 @@
 package objects;
 
 import haxe.Json;
-  
+
+typedef TileJson = {
+    playerPosition:{x:Float, y:Float};
+    map:{width:Int, height:Int};
+    tiles:Array<{x:Float, y:Float, image:String}>;
+}
 class Tile extends FlxSpriteGroup {
-    var tileJson;
+    public var tileData:TileJson;
     public function new(x:Float, y:Float, tilePath:String) {
         super(x, y);
 
-        tileJson = Json.parse(Paths.getpath('data/tiles/$tilePath.json'));
-        for (tileArray in tileJson.tiles) {
-            var tileSpr:FlxSprite = new FlxSprite(tileArray.x, tilearray.y, Paths.image(tileArray.image));
+        tileData = Json.parse(Paths.getTextFromFile('data/tiles/$tilePath.json'));
+        for (tileArray in tileData.tiles) {
+            var tileSpr:FlxSprite = new FlxSprite(tileArray.x, tileArray.y, Paths.image(tileArray.image));
             tileSpr.immovable = true;
             add(tileSpr);
         }
