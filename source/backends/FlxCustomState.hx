f30c5b5c9961838064797f04953c72d5711b79e6
kivers69 <georgechen1297@gmail.com>
2024-09-12 21:25:26 +0800

    deleted instance on custom state

--- source/backends/FlxCustomState.hx
+++ source/backends/FlxCustomState.hx
@@ -5,14 +5,7 @@ class FlxCustomState extends FlxState {
     public var onCreatePost:() -> Void;
     public var onUpdate:(Float) -> Void;
     public var onUpdatePost:(Float) -> Void;
-
-    public static var instance:FlxCustomState;
-
-    public function new() {
-        instance = this;
-        super();
-    }
-
+    
     override function create() {
         if (onCreate != null) onCreate();
         super.create();
