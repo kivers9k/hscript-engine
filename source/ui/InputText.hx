d966cd0588b241ccefda7603ee8db942c7fef018
kivers69 <georgechen1297@gmail.com>
2024-09-15 17:03:28 +0800

    aughhh

--- source/ui/InputText.hx
+++ source/ui/InputText.hx
@@ -1,34 +1,39 @@
 package ui;
 
 class InputText extends FlxUIGroup { 
+    private var label:FlxText;
+    private var inputText:FlxInputText;
+    
     public var text(get, set):String;
 
     public function new(x:Float, y:Float, widthField:Int, text:String, name:String = 'no name') {
         super(x, y);
 
-        var label:FlxText = new FlxText(0, 0, widthField, name);
+        label = new FlxText(0, 0, widthField, name);
         label.scrollFactor.set();
         add(label);
 
-        var textBar:FlxInputText = new FlxInputText(0, 20, widthField, text);
-        textBar.scrollFactor.set();
-        add(textBar);
+        inputText = new FlxInputText(0, 20, widthField, text);
+        inputText.scrollFactor.set();
+        add(inputText);
 
         scrollFactor.set();
         this.name = name;
     }
 
     public function resize(w:Float, h:Float):Void {
-        members[1].fieldWidth = w;
-        members[2].width = w;
-        members[2].height = h;
-        members[2].calcFrame();
+        label.fieldWidth = w;
+        inputText.width = w;
+        inputText.height = h;
+        inputText.calcFrame();
     }
 
     private function set_text(txt:String):String {
-        members[2].text = txt;
-        return members[2].text;
+        inputText.text = txt;
+        return inputText.text;
     }
 
-    private function get_text():String return members[2].text;
+    private function get_text():String {
+        return inputText.text;
+    }
 }
