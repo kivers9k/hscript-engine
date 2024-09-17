64d825fb7ebffad75562266e2e6963e51f13ad88
kivers69 <georgechen1297@gmail.com>
2024-09-17 16:46:50 +0800

    man

--- source/ui/FlxVirtualPad.hx
+++ source/ui/FlxVirtualPad.hx
@@ -4,6 +4,9 @@ class FlxVirtualPad extends FlxUIGroup {
     public var dPad:FlxUIGroup;
     public var actions:FlxUIGroup;
 
+    private static var buttonName:Array<String> = [];
+    public var fromString:Map<String, FlxButton>;
+
     public static var pressed = {};
     public static var justPressed = {};
     public static var justReleased = {};
@@ -17,13 +20,42 @@ class FlxVirtualPad extends FlxUIGroup {
 
         actions = new FlxUIGroup();
         actions.scrollFactor.set();
+
+        switch (dpad) {
+            case UP_DOWN:
+            case LEFT_RIGHT:
+            case FULL:
+        }
+
+        switch (actions) {
+            case A:
+            case A_B:
+            case A_B_C:
+            case A_B_X_Y:
+        }
     }
 
     public function createButton(x:Float, y:Float, ?w:Int = 132, ?h:Int = 135, frame:String) {
         var button:FlxSpriteButton = new FlxSpriteButton(x, y);
         button.frames = Paths.fromFrame('ui/virtual-input', frame, w, h);
         button.scrollFactor.set();
+
+        buttonName.push(frame.toUpperCase());
+        fromString.set(frame, button);
+
         return button;
+    }
+
+    override public function update(elapsed:Float):Void {
+        super.update(elapsed);
+
+        for (name in buttonName) {
+            if (fromString.get(name) != null) {
+                pressed = {"$name": fromString.get(name).pressed};
+                justPressed = {"$name": fromString.get(name).justPressed};
+                justReleased = {"$name": fromString.get(name).justReleased};
+            }
+        }
     }
 
     override public function destroy():Void {
@@ -38,8 +70,8 @@ class FlxVirtualPad extends FlxUIGroup {
 }
 
 enum FlxDPadMode {
-    LEFT_RIGHT;
     UP_DOWN;
+    LEFT_RIGHT;
     FULL;
 }
 
