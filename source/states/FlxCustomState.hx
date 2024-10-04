94af20f4abc72017ae6bebbe39ce8ff3e18b003f
kivers69 <georgechen1297@gmail.com>
2024-10-04 19:18:17 +0800

    update source/states/FlxCustomState.hx

--- source/states/FlxCustomState.hx
+++ source/states/FlxCustomState.hx
@@ -1,41 +1,41 @@
 package states;
 
 class FlxCustomState extends FlxState {
-    var hscript:HScript;
-    public static var instance:FlxCustomState;
+	var hscript:HScript;
+	public static var instance:FlxCustomState;
 
-    public function new(stateName:String) {
-        instance = this;
+	public function new(stateName:String) {
+		instance = this;
 
-        if (FileSystem.exists(Paths.getPath('states/$stateName.hx')) && stateName != null)
-            hscript = new HScript(Paths.getPath('states/$stateName.hx'));
+		if (FileSystem.exists(Paths.getPath('states/$stateName.hx')) && stateName != null)
+			hscript = new HScript(Paths.getPath('states/$stateName.hx'));
 
-        super();
-    }
+		super();
+	}
 
-    override function create() {
-        hscript.call('onCreate', []);
+	override function create() {
+		hscript.call('onCreate', []);
 
-        super.create();
+		super.create();
 
-        hscript.call('onCreatePost', []);
-    }
+		hscript.call('onCreatePost', []);
+	}
 
-    override function update(elapsed:Float) {
-        hscript.call('onUpdate', [elapsed]);
+	override function update(elapsed:Float) {
+		hscript.call('onUpdate', [elapsed]);
 
-        super.update(elapsed);
+		super.update(elapsed);
 
-        hscript.call('onUpdatePost', [elapsed]);
-    }
+		hscript.call('onUpdatePost', [elapsed]);
+	}
 
-    override function destroy() {
-        if (hscript != null) {
-            hscript.call('onDestroy', []);
-            hscript.close();
-            hscript = null;
-        }
+	override function destroy() {
+		if (hscript != null) {
+			hscript.call('onDestroy', []);
+			hscript.stop();
+			hscript = null;
+		}
 
-        super.destroy();
-    }
+		super.destroy();
+	}
 }
