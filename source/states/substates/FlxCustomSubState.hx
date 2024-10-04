1edaf149f687dc73cf02e6be15844f0c730074ea
kivers69 <georgechen1297@gmail.com>
2024-10-04 19:18:22 +0800

    update source/states/substates/FlxCustomSubState.hx

--- source/states/substates/FlxCustomSubState.hx
+++ source/states/substates/FlxCustomSubState.hx
@@ -1,44 +1,44 @@
 package states.substates;
 
 class FlxCustomSubState extends FlxSubState {
-    var hscript:HScript;
-    public static var instance:FlxCustomSubState;
-
-    public function new(subStateName:String) {
-        instance = this;
-        
-        if (FileSystem.exists(Paths.getPath('subStates/$subStateName.hx')) && subStateName != null) {
-            hscript = new HScript(Paths.getPath('subStates/$subStateName.hx'));
-        } else {
-            close();
-        }
-
-        super();
-    }
-
-    override function create() {
-        hscript.call('onCreate', []);
-
-        super.create();
-
-        hscript.call('onCreatePost', []);
-    }
-
-    override function update(elapsed:Float) {
-        hscript.call('onUpdate', [elapsed]);
-
-        super.update(elapsed);
-
-        hscript.call('onUpdatePost', [elapsed]);
-    }
-
-    override function destroy() {
-        if (hscript != null) {
-            hscript.call('onDestroy', []);
-            hscript.close();
-            hscript = null;
-        }
-        
-        super.destroy();
-    }
+	var hscript:HScript;
+	public static var instance:FlxCustomSubState;
+	
+	public function new(subStateName:String) {
+		instance = this;
+		
+		if (FileSystem.exists(Paths.getPath('subStates/$subStateName.hx')) && subStateName != null) {
+			hscript = new HScript(Paths.getPath('subStates/$subStateName.hx'));
+		} else {
+			close();
+		}
+		
+		super();
+	}
+	
+	override function create() {
+		hscript.call('onCreate', []);
+		
+		super.create();
+		
+		hscript.call('onCreatePost', []);
+	}
+	
+	override function update(elapsed:Float) {
+		hscript.call('onUpdate', [elapsed]);
+		
+		super.update(elapsed);
+		
+		hscript.call('onUpdatePost', [elapsed]);
+	}
+	
+	override function destroy() {
+		if (hscript != null) {
+			hscript.call('onDestroy', []);
+			hscript.close();
+			hscript = null;
+		}
+		
+		super.destroy();
+	}
 }
