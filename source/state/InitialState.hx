f2edbc8d467a8db3f886bd5a46c2b5481ea6b47f
kivers69 <georgechen1297@gmail.com>
2024-10-12 15:55:38 +0000

    it should be FlxState

--- source/state/InitialState.hx
+++ source/state/InitialState.hx
@@ -1,6 +1,6 @@
 package state;
 
-class InitialState extends GameState {
+class InitialState extends FlxState {
     override function create() {
         var stateName:String = File.getContent(SUtil.getPath('initialState.txt'));
 		FlxG.switchState(new FlxCustomState(stateName.split('\n')[0].trim()));
