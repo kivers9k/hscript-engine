e3d450927e4895e1f1ec9797b66d6c7584615ac9
kivers69 <georgechen1297@gmail.com>
2025-02-11 02:44:10 +0000

    uodate instance

--- source/state/GameState.hx
+++ source/state/GameState.hx
@@ -2,14 +2,9 @@ package state;
 
 // state example
 class GameState extends FlxState { 
-    public static var instance:GameState;
+    public static var instance:GameState = this;
     public var hscript:HScript;
     public var camHUD:FlxCamera;
-
-    public function new() {
-        instance = this;
-        super();
-    }
 
     override function create() {
         camHUD = new FlxCamera();
--- source/state/GameSubState.hx
+++ source/state/GameSubState.hx
@@ -2,11 +2,6 @@ package state;
 
 //another substate example
 class GameSubState extends FlxSubState {
-    public static var instance:GameSubState;
+    public static var instance:GameSubState = this;
     public var hscript:HScript;
-
-    public function new() {
-        instance = this;
-        super();
-    }
 }
