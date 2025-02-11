07a41010020116adbf7d7b675048ef74ce82a4eb
kivers69 <georgechen1297@gmail.com>
2025-02-11 05:11:03 +0000

    Revert "uodate instance"
    This reverts commit e3d450927e4895e1f1ec9797b66d6c7584615ac9.

--- source/state/GameState.hx
+++ source/state/GameState.hx
@@ -2,9 +2,14 @@ package state;
 
 // state example
 class GameState extends FlxState { 
-    public static var instance:GameState = this;
+    public static var instance:GameState;
     public var hscript:HScript;
     public var camHUD:FlxCamera;
+
+    public function new() {
+        instance = this;
+        super();
+    }
 
     override function create() {
         camHUD = new FlxCamera();
--- source/state/GameSubState.hx
+++ source/state/GameSubState.hx
@@ -2,6 +2,11 @@ package state;
 
 //another substate example
 class GameSubState extends FlxSubState {
-    public static var instance:GameSubState = this;
+    public static var instance:GameSubState;
     public var hscript:HScript;
+
+    public function new() {
+        instance = this;
+        super();
+    }
 }
