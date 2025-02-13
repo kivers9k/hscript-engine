88ebbc5c7566e9931ed5bfe57aab39c9ff4417e6
kivers69 <georgechen1297@gmail.com>
2025-02-13 14:34:47 +0800

    shit

--- source/script/HScript.hx
+++ source/script/HScript.hx
@@ -63,6 +63,7 @@ class HScript {
 		setVariable('FlxCustomSubState', FlxCustomSubState);
 		setVariable('Paths', Paths);
 		setVariable('SUtil', SUtil);
+		setVariable('ColorUtil', ColorUtil);
 
 		// state variable
 		var state = GameState.instance;
