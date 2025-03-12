bb71fb987317e7bd91d02548bc1e6a05e64ae1fb
kivers69 <georgechen1297@gmail.com>
2025-03-12 18:17:22 +0800

    let try inlune

--- source/backend/CustomPath.hx
+++ source/backend/CustomPath.hx
@@ -17,11 +17,11 @@ class CustomPath {
 		return SUtil.getPath('$paths/$key');
 	}
 
-	inline public static function exists(key:String):Bool {
+	public inline static function exists(key:String):Bool {
 		return FileSystem.exists(getPath(key));
 	}
 
-	inline public static function image(key:String, ?imgFolder:String = 'images'):FlxGraphic {
+	public inline static function image(key:String, ?imgFolder:String = 'images'):FlxGraphic {
 		if (exists('$imgFolder/$key.png')) {
 			var bitmap:BitmapData = BitmapData.fromFile(getPath('images/$key.png'));
 			var graphic:FlxGraphic = FlxGraphic.fromBitmapData(bitmap, false, getPath('images/$key.png'));
@@ -31,42 +31,42 @@ class CustomPath {
 		return null;
 	}
 	
-	public static function sound(key:String, ?sndFolder:String = 'sounds'):Sound {
+	public inline function sound(key:String, ?sndFolder:String = 'sounds'):Sound {
 		if (exists('$sndFolder/$key.ogg')) {
 			return Sound.fromFile(getPath('$sndFolder/$key.ogg'));
 		}
 		return null;
 	}
     
-	public static function font(key:String, ?fontFolder:String = 'fonts'):String {
+	public inline function font(key:String, ?fontFolder:String = 'fonts'):String {
 		if (exists('$fontFolder/$key') && (key.endsWith('.ttf') || key.endsWith('.otf'))) {
 			return getPath('$fontFolder/$key');
 		}
         return null;
     }
     
-	public static function getContent(key:String):String {
+	public inline function getContent(key:String):String {
 		if (exists(key)) {
 			return File.getContent(getPath(key));
 		}
 		return null;
     }
 
-	public static function json(key:String):Dynamic {
+	public inline function json(key:String):Dynamic {
 		if (exists('$key.json')) {
 			return haxe.Json.parse(getContent('$key.json'));
 		}
 		return null;
 	}
 
-	public static function shader(key:String, ?shaderFolder:String = 'shaders'):FlxRuntimeShader {
+	public inline function shader(key:String, ?shaderFolder:String = 'shaders'):FlxRuntimeShader {
  		if (exists('$shaderFolder/$key') && (key.endsWith('.frag') || key.endsWith('.vert'))) {
 			return new FlxRuntimeShader(getContent('$shaderFolder/$key'));
 		}
 		return null;
 	}
 
-	public static function getFrame(key:String, ?path:String = 'images', ?type:String = 'sparrow'):FlxAtlasFrames {
+	public inline function getFrame(key:String, ?path:String = 'images', ?type:String = 'sparrow'):FlxAtlasFrames {
 		var fileFormat:String = null;
 		switch (type) {
             case 'sparrow': fileFormat = 'xml';
@@ -84,7 +84,7 @@ class CustomPath {
 		return null;
 	}
 
-	public static function fromFrame(key:String, ?path:String = 'images', frame:String, w:Int, h:Int, ?type = 'sparrow'):FlxTileFrames {
+	public inline function fromFrame(key:String, ?path:String = 'images', frame:String, w:Int, h:Int, ?type = 'sparrow'):FlxTileFrames {
         if (exists('$path/$key.png')) {
             return FlxTileFrames.fromFrame(getFrame(key, path, type).getByName(frame), FlxPoint.get(w, h));
 		}
