bf8af96afd10d1f4981dffa94c70bf8b3edf88f8
kivers69 <georgechen1297@gmail.com>
2025-03-28 13:03:17 +0800

    update HScript

--- source/script/HScript.hx
+++ source/script/HScript.hx
@@ -3,6 +3,10 @@ package script;
 class HScript {
 	public var parser:Parser = new Parser();
 	public var interp:Interp = new Interp();
+
+	public var variables(get, null):Map<String, Dynamic>;
+	public var modules(get, null):Map<String, Dynamic>;
+
 	public var scriptName:String;
 
 	public function new(hxPath:String, ?name:String, ?preset:Bool = false) {
@@ -31,35 +35,35 @@ class HScript {
 
 	public function presetVars() {
 		// default set
-		setVariable('StringTools', StringTools);
-		setVariable('Reflect', Reflect);
-		setVariable('Math', Math);
-		setVariable('Type', Type);
-		setVariable('Std', Std);
-		setVariable('this', this);
+		modules.set('StringTools', StringTools);
+		modules.set('Reflect', Reflect);
+		modules.set('Math', Math);
+		modules.set('Type', Type);
+		modules.set('Std', Std);
+		modules.set('this', this);
 
 		// flixel class
-		setVariable('FlxG', FlxG);
-		setVariable('FlxSprite', FlxSprite);
-		setVariable('FlxCamera', FlxCamera);
-		setVariable('FlxTween', FlxTween);
-		setVariable('FlxEase', FlxEase);
-		setVariable('FlxTimer', FlxTimer);
-		setVariable('FlxText', FlxText);
+		modules.set('FlxG', FlxG);
+		modules.set('FlxSprite', FlxSprite);
+		modules.set('FlxCamera', FlxCamera);
+		modules.set('FlxTween', FlxTween);
+		modules.set('FlxEase', FlxEase);
+		modules.set('FlxTimer', FlxTimer);
+		modules.set('FlxText', FlxText);
 
 		// source class
-		setVariable('GameState', GameState);
-		setVariable('GameSubState', GameSubState);
-		setVariable('FlxCustomState', FlxCustomState);
-		setVariable('FlxCustomSubState', FlxCustomSubState);
-		setVariable('Main', Main);
-		setVariable('Paths', Paths);
-		setVariable('Files', Files);
-		setVariable('SUtil', SUtil);
+		modules.set('GameState', GameState);
+		modules.set('GameSubState', GameSubState);
+		modules.set('FlxCustomState', FlxCustomState);
+		modules.set('FlxCustomSubState', FlxCustomSubState);
+		modules.set('Main', Main);
+		modules.set('Paths', Paths);
+		modules.set('Files', Files);
+		modules.set('SUtil', SUtil);
         
 		// shader
-		setVariable('FlxRuntimeShader', FlxRuntimeShader);
-		setVariable('ShaderFilter', openfl.filters.ShaderFilter);
+		modules.set('FlxRuntimeShader', FlxRuntimeShader);
+		modules.set('ShaderFilter', openfl.filters.ShaderFilter);
 
 		// state variable
 		var state = GameState.instance;
@@ -67,14 +71,14 @@ class HScript {
 		    var state = GameSubState.instance;
 		}
 
-		setVariable('game', state);
-		setVariable('add', state.add);
-		setVariable('remove', state.remove);
-		setVariable('insert', state.insert);
-		setVariable('members', state.members);
+		variables.set('game', state);
+		variables.set('add', state.add);
+		variables.set('remove', state.remove);
+		variables.set('insert', state.insert);
+		variables.set('members', state.members);
 
         // subState variables
-		setVariable('close', GameSubState.instance.close);
+		variables.set('close', GameSubState.instance.close);
 	}
 
 	public function execute(code:String):Dynamic {
@@ -82,13 +86,14 @@ class HScript {
 		    return interp.execute(parser.parseString(code));
 		} catch(e:Dynamic) {
 		    SUtil.alert('Error on HScript', 'in $scriptName\n$e');
+			trace(e);
 		}
 		return null;
 	}
 
 	public function call(name:String, args:Array<Dynamic>):Dynamic {
-		if (existsVariable(name)) {
-			var func = getVariable(name);
+		if (variables.exists(name)) {
+			var func = variables.get(name);
 			if (Reflect.isFunction(func)) {
 				var returnFunc = null;
 				try {
@@ -104,28 +109,17 @@ class HScript {
 	}
 
 	public function stop():Void {
-		if (interp != null && interp.variables != null) {
-			interp.variables.clear();
+		if (interp != null && variables != null) {
+			variables.clear();
 			interp = null;
 		}
 	}
 
-	public function setVariable(name:String, args:Dynamic) {
-		interp.variables.set(name, args);
+	private function get_variables() {
+		return interp.variables;
 	}
 
-	public function getVariable(name:String):Dynamic {
-	    if (interp.variables.exists(name)) {
-			return interp.variables.get(name);
-		}
-		return null;
-	}
-
-	public function removeVariable(name:String) {
-		interp.variables.remove(name);
-	}
-
-	public function existsVariable(name:String):Bool {
-		return interp.variables.exists(name);
+	private function get_modules() {
+		return interp.modules;
 	}
 }
