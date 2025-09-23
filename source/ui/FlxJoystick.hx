92cd5e485d8a38aa144ea0a3d5a0999c28eacef9
kivers69 <georgechen1297@gmail.com>
2025-09-24 01:37:31 +0800

    test

--- source/ui/FlxJoystick.hx
+++ source/ui/FlxJoystick.hx
@@ -1,14 +1,10 @@
 package ui;
 
 import flixel.math.FlxPoint;
-import flixel.input.touch.FlxTouch;
-import flixel.system.FlxAssets.FlxGraphicAsset;
 
 class FlxJoystick extends FlxSpriteGroup {
 	var base:FlxSprite;
 	var thumb:FlxSprite;
-	
-	var touch:FlxTouch;
 	
 	var direction:Float = 0;
 	var amount:Float = 0;
@@ -19,58 +15,77 @@ class FlxJoystick extends FlxSpriteGroup {
 	public var stickX(get, default):Int = 0;
 	public var stickY(get, default):Int = 0;
 
-	public function new(X:Float = 0, Y:Float = 0, ?Radius:Float = 90, ?baseGraphic:Null<FlxGraphicAsset>, ?thumbGraphic:Null<FlxGraphicAsset>) {
-		super(x, y);
+	public function new(?X:Float = 0, ?Y:Float = 0, ?Radius:Float = 90, ?baseSprite:FlxSprite, ?thumbSprite:FlxSprite) {
+		super(X, Y);
 		
-		base = new FlxSprite(0, 0, baseGraphic);
 		if (baseGraphic == null) {
+			base = new FlxSprite();
 			base.frames = Paths.getFrame('ui/virtual-input', 'packer');
 			base.animation.frameName = 'base';
-			base.resetSizeFromFrame();
+		} else {
+			base = baseSprite;
 		}
+		base.resetSizeFromFrame();
 		base.moves = false;
 		add(base);
 		
-		thumb = new FlxSprite(0, 0, thumbGraphic);
 		if (thumbGraphic == null) {
+			thumb = new FlxSprite();
 			thumb.frames = Paths.getFrame('ui/virtual-input', 'packer');
 			thumb.animation.frameName = 'thumb';
-			thumb.resetSizeFromFrame();
+		} else {
+			thumb = thumbSprite;
 		}
+		thumb.resetSizeFromFrame();
 		thumb.moves = false;
 		add(thumb);
 		
 		this.radius = Radius;
 		this.scrollFactor.set();
 	}
-	
-	var _touched:Bool = false;
+
 	override function update(elapsed:Float):Void {
 		super.update(elapsed);
-		
-		if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(base, cameras[0]) && visible) {
-			_touched = true;
-		} else if (FlxG.mouse.justReleased) {
-			_touched = false;
+
+        if (visible) {
+		    updateJoystick();
 		}
-		
-		if (_touched) {
-			var _touchPoint:FlxPoint = FlxG.mouse.getScreenPosition(cameras[0]);
-			var dx:Float = _touchPoint.x - base.x - (base.width / 2);
-			var dy:Float = _touchPoint.y - base.y - (base.height / 2);
-			var dist:Float = Math.sqrt(dx * dx + dy * dy);
-			if (dist < 1) dist = 0;
+	}
+
+	public funtion joystickScale(?x:Float = 1, ?y:Float = 1) {
+		for (spr in members) {
+		    spr.scale.set(x, y);
+		    spr.updateHitbox();
+		}
+	}
+	
+	var _touched:Bool = false;
+	function updateJoystick():Void {
+		for (touch in FlxG.touches.list) {
+		    if (touch.overlaps(base, camera) && touch.pressed) {
+		    	_touched = true;
+	    	} else if (touch.justReleased) {
+	    		_touched = false;
+	    	}
+    		
+	    	if (_touched) {
+        		var _touchPoint:FlxPoint = touch.getScreenPosition(camera);
+		    	var dx:Float = _touchPoint.x - base.x - (base.width / 2);
+	    		var dy:Float = _touchPoint.y - base.y - (base.height / 2);
+	    		var dist:Float = Math.sqrt(dx * dx + dy * dy);
+		    	if (dist < 1) dist = 0;
 			
-			direction = Math.atan2(dy, dx);
-			amount = Math.min(radius, dist) / radius;
+	    		direction = Math.atan2(dy, dx);
+	    		amount = Math.min(radius, dist) / radius;
 			
-			thumb.x = x + (base.width / 2) + Math.cos(direction) * amount * radius - (thumb.width / 2);
-			thumb.y = y + (base.height / 2) + Math.sin(direction) * amount * radius - (thumb.height / 2);
-		} else {
-			thumb.x = base.x + (base.width - thumb.width) / 2;
-			thumb.y = base.y + (base.height - thumb.height) / 2;
-			direction = 0;
-			amount = 0;
+	    		thumb.x = x + (base.width / 2) + Math.cos(direction) * amount * radius - (thumb.width / 2);
+	    		thumb.y = y + (base.height / 2) + Math.sin(direction) * amount * radius - (thumb.height / 2);
+	    	} else {
+	    		thumb.x = base.x + (base.width - thumb.width) / 2;
+	    		thumb.y = base.y + (base.height - thumb.height) / 2;
+	    		direction = 0;
+	    		amount = 0;
+	    	}
 		}
 	}
 
